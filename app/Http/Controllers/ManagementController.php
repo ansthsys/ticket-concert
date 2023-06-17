30bb63c0593547cf2b7efe4bb2d4dea75c3451f1
ansthsys <azmiythufail91@gmail.com>
2023-06-17 17:04:57 +0700

    feat: create method returning list buyer ticket

---
+++ app/Http/Controllers/ManagementController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ManagementController extends Controller
+{
+    public function getBuyer()
+    {
+        $res = Ticket::with('concert')->with('user')->get();
+
+        return view('buyer', [
+            'res' => $res
+        ]);
+    }
+}
