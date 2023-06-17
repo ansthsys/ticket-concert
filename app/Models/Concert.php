de7574efc2af29b7de63d0c06f5796b152647aac
ansthsys <azmiythufail91@gmail.com>
2023-06-17 14:18:15 +0700

    feat: create concert model

--- app/Models/Concert.php
+++ app/Models/Concert.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Concert extends Model
@@ -13,4 +14,9 @@ class Concert extends Model
         'name',
         'scheduled_date',
     ];
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class, 'concert_id', 'id');
+    }
 }
