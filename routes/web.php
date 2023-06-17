352189d0a57a678367fffe840940bd0f5950e8bd
ansthsys <azmiythufail91@gmail.com>
2023-06-17 17:37:53 +0700

    feat: add route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\ConcertController;
+use App\Http\Controllers\ManagementController;
+use App\Http\Middleware\AdminAccess;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +20,7 @@ use App\Http\Controllers\ConcertController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('landing');
 
 Auth::routes();
 
@@ -28,4 +30,14 @@ Route::get('/concert', [ConcertController::class, 'getConcert'])->name('concert'
 Route::middleware(['auth'])->group(function () {
     Route::get('/ticket', [TicketController::class, 'getMyTicket'])->name('myTicket');
     Route::post('/ticket/buy', [TicketController::class, 'buyTicket'])->name('buyTicket');
+
+    // Management
+    Route::middleware([AdminAccess::class])->group(function () {
+        Route::get('/buyer', [ManagementController::class, 'getBuyer'])->name('management.buyer');
+        Route::get('/buyer/{id}', [ManagementController::class, 'getSingleBuyer'])->name('management.buyer.edit');
+        Route::post('/buyer/{id}', [ManagementController::class, 'updateSingleBuyer'])->name('management.buyer.update');
+        Route::post('/buyer/delete', [ManagementController::class, 'deleteBuyer'])->name('management.buyer.delete');
+        Route::get('/process', [ManagementController::class, 'getProcess'])->name('management.process');
+        Route::post('/process', [ManagementController::class, 'postProcess'])->name('management.post.process');
+    });
 });
