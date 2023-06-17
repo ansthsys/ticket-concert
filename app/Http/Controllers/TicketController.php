7de84701bd42001decdbcda7a996bef6b0527413
ansthsys <azmiythufail91@gmail.com>
2023-06-17 14:23:53 +0700

    feat: create method for handle buying ticket

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TicketController extends Controller
+{
+    public function buyTicket(Request $request)
+    {
+        $user_id = Auth::id();
+        $concert_id = $request->concert_id;
+        $code = uniqid('TIX' . rand(1000, 9999) . '-', true);
+
+        $data = Ticket::create([
+            'user_id' => $user_id,
+            'concert_id' => $concert_id,
+            'code' => $code,
+        ]);
+
+        return redirect()->route('myTicket');
+    }
+}
